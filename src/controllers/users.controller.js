762ae5fc9e128fa23ff5fb854ddea1c8b5e43511
prayag <prayag129787@gmail.com>
2024-01-30 20:18:01 +0530

    change in login route username to email

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -50,11 +50,11 @@ export const registerUser = async (req, res) => {
 
 export const loginUser = async (req, res) => {
     try {
-        const { username, password } = req.body;
-        if (!username || !password)
+        const { email, password } = req.body;
+        if (!email || !password)
             return res.status(400).json({ message: "Please fill all fields" });
 
-        const user = await User.findOne({ username }).select("+password");
+        const user = await User.findOne({ email }).select("+password");
 
         if (!user) return res.status(400).json({ message: "User not found" });
 
